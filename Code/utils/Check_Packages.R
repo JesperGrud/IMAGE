dab9164536ea5293e81cc4290fc21db74a9ec578
JesperGrud <jgsm@bmb.sdu.dk>
2021-01-06 11:20:26 +0100

    Create Check_Packages.R

---
+++ Code/utils/Check_Packages.R
@@ -0,0 +1,42 @@
+#!/usr/bin/Rscript
+
+if (is.element('glmnet', installed.packages()[,1]) == F) {
+cat("FALSE")
+}
+
+if (is.element('methods', installed.packages()[,1]) == F) {
+cat("FALSE")
+}
+
+if (is.element('cluster', installed.packages()[,1]) == F) {
+cat("FALSE")
+}
+
+if (is.element('doParallel', installed.packages()[,1]) == F) {
+cat("FALSE")
+}
+
+if (is.element('foreach', installed.packages()[,1]) == F) {
+cat("FALSE")
+}
+
+if (is.element('Matrix', installed.packages()[,1]) == F) {
+cat("FALSE")
+}
+
+if (is.element('data.table', installed.packages()[,1]) == F) {
+cat("FALSE")
+}
+
+if (is.element('GenomicRanges', installed.packages()[,1]) == F) {
+cat("FALSE")
+}
+
+if (is.element('edgeR', installed.packages()[,1]) == F) {
+cat("FALSE")
+}
+
+
+
+
+
